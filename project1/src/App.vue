37f053913327751d5e8af7f90d070d36a3d2b65d
derek-alves <derek.alves@fatec.sp.gov.br>
2020-09-18 13:56:13 -0300

    Initial project commit

---
+++ project1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Guia do Derek</h1>
+    <Cliente/>
+  </div>
+</template>
+
+<script>
+import Cliente from './components/Cliente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Cliente
+  }
+}
+</script>
+
---
+++ project1/src/components/Cliente.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="cliente">
+    <h2>Derek vue</h2>
+    <hr>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #cliente{
+      color:blue;
+      background:black;
+      max-width: 600px;
+      height: 400px;
+    }
+</style>
